2a7e30f82c84986fbfec28ed19909a329b0256cd
pruthvi <pruthvid.mys@gmail.com>
2018-09-18 11:20:43 +0530

    first commit

---
+++ fibo.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+
+    int i,n,temp1=0,temp2=0,nt;
+    printf("enter n\n");
+    scanf("%d",&n);
+    printf("fibonacci series for n numbers\n");
+    for(i=1;i<=n;i++)
+    {
+
+    if (i<=1)
+    nt=i;
+    else
+    {
+        nt=temp1+temp2;
+        temp1=temp2;
+        temp2=nt;
+    }
+    printf("%d\n",nt);
+    return 0;
+}
