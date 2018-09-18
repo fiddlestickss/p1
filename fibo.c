2102986b8a1b43fd464043d0cde32cae1e6e1ce4
fiddlestickss <43372236+fiddlestickss@users.noreply.github.com>
2018-09-18 16:24:56 +0530

    Update fibo.c

--- fibo.c
+++ fibo.c
@@ -17,6 +17,6 @@ int main()
         temp1=temp2;
         temp2=nt;
     }
-    printf("fibonacci series for n numbers %d\n",nt);
+    printf("fibonacci series for n number''s %d\n",nt);
     return 0;
 }
